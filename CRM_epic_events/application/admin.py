da875871ddcab5450581d6acac493381f2ac666f
lcourdes <lea.courdes.murphy@gmail.com>
2023-03-13 21:22:56 +0100

    display users on administration site

--- CRM_epic_events/application/admin.py
+++ CRM_epic_events/application/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from application.models import User
 
-# Register your models here.
+class UserAdministrators(UserAdmin):
+    list_display = ('id', 'username', 'last_name', 'first_name', 'email')  
+    fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password', 'first_name', 
+                       'last_name', 'groups', 'is_staff')}
+        ),
+    )
+    list_filter = ['last_name', 'groups']
+
+admin.site.register(User, UserAdministrators)
