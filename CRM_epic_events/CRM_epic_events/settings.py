d1f95afe32c354b6af485016064843d94c055835
lcourdes <lea.courdes.murphy@gmail.com>
2023-03-14 16:33:00 +0100

    actualize settings for drf

--- CRM_epic_events/CRM_epic_events/settings.py
+++ CRM_epic_events/CRM_epic_events/settings.py
@@ -27,6 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'application.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+}
 
 # Application definition
 
@@ -38,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'application'
 ]
 
