ad7619531b5445bc5b498d4bf2365c47264a1445
lcourdes <lea.courdes.murphy@gmail.com>
2023-03-15 18:28:48 +0100

    modifying permissions

--- CRM_epic_events/application/permissions.py
+++ CRM_epic_events/application/permissions.py
@@ -1,6 +1,27 @@
 from rest_framework.permissions import BasePermission
 
-class IsSaler(BasePermission):        
+class IsSaler(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name='salers').exists():
+            if view.basename == 'events':
+                if request.method == 'POST':
+                    return True
+    
     def has_object_permission(self, request, view, obj):
-        if obj.sales_contact == request.user:
+        if view.basename == 'contracts':
+            if obj.sales_contact == request.user:
                 return True
+        
+
+class IsSupport(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name='supporters').exists():
+            if view.basename == 'events':
+                if request.method == 'PUT':
+                    return True
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name='supporters').exists():
+            if view.basename == 'events':
+                if request.method == 'PUT':
+                    if obj.support_contact == request.user:
+                        return True
